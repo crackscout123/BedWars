e7e81513b3ebe6b87303151a92b16ec506215e3b
getPoland <rzepka.joel@outlook.de>
2025-06-21 02:38:28 +0200

    Initial commit

---
+++ src/main/java/eu/crackscout/bedwars/Main.java
@@ -0,0 +1,26 @@
+package eu.crackscout.bedwars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		System.out.println("hello world :3");
+	}
+	
+	@Override
+	public void onDisable() {
+		System.out.println("bye bye world!");
+	}
+	
+}
+
+
+/** 
+ *
+ * @author Joel Rzepka - crackscout.de
+ *
+ * @date Jun 21, 2025 - 2:32:23 AM
+ *
+ */
